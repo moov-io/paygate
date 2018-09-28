f56e0947668e0afb5c9b23d8788b40cd303606c6
Adam Shannon <adamkshannon@gmail.com>
2018-09-28 16:11:46 -0500

    http: quick test over paygateResponseWriter

---
+++ http_test.go
@@ -0,0 +1,42 @@
+// Copyright 2018 The Paygate Authors
+// Use of this source code is governed by an Apache License
+// license that can be found in the LICENSE file.
+
+package main
+
+import (
+	"net/http/httptest"
+	"testing"
+)
+
+func TestHttp__paygateResponseWriter(t *testing.T) {
+	// missing x-user-id
+	r := httptest.NewRequest("GET", "/testing", nil)
+	r.Header.Set("x-user-id", "")
+
+	w := httptest.NewRecorder()
+	_, err := wrapResponseWriter(w, r, nil, "testing")
+	if err == nil {
+		t.Error("expected error")
+	}
+
+	w.Flush()
+	if w.Code != 403 {
+		t.Errorf("got %d", w.Code)
+	}
+
+	// success with x-user-id
+	r = httptest.NewRequest("GET", "/testing", nil)
+	r.Header.Set("x-user-id", "my-user-id")
+
+	w = httptest.NewRecorder()
+	_, err = wrapResponseWriter(w, r, nil, "testing")
+	if err != nil {
+		t.Error(err)
+	}
+
+	w.Flush()
+	if w.Code != 200 {
+		t.Errorf("got %d", w.Code)
+	}
+}

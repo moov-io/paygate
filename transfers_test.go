dbd17083dce8152ca65d7dece87c016b27c19bb3
Adam Shannon <adamkshannon@gmail.com>
2018-10-01 13:22:53 -0500

    transfers: add quick test

--- transfers_test.go
+++ transfers_test.go
@@ -7,6 +7,7 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"net/http/httptest"
 	"testing"
 )
 
@@ -56,5 +57,29 @@ func TestTransferStatus__json(t *testing.T) {
 	in := []byte(fmt.Sprintf(`"%v"`, nextID()))
 	if err := json.Unmarshal(in, &ts); err == nil {
 		t.Error("expected error")
+	}
+}
+
+func TestTransfers__getUserTransfers(t *testing.T) {
+	w := httptest.NewRecorder()
+	r := httptest.NewRequest("GET", "/transfers", nil)
+	r.Header.Set("x-user-id", "test")
+
+	getUserTransfers(memTransferRepo{})(w, r)
+	w.Flush()
+
+	if w.Code != 200 {
+		t.Errorf("got %d", w.Code)
+	}
+
+	var transfers []*Transfer
+	if err := json.Unmarshal(w.Body.Bytes(), &transfers); err != nil {
+		t.Error(err)
+	}
+	if len(transfers) != 1 {
+		t.Errorf("got %d transfers=%v", len(transfers), transfers)
+	}
+	if transfers[0].ID == "" {
+		t.Errorf("transfers[0]=%v", transfers[0])
 	}
 }

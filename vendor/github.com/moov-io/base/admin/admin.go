23731c02ad05efbda3c62c4178451734236039a0
Adam Shannon <adamkshannon@gmail.com>
2018-10-30 13:32:57 -0700

    vendor deps

--- vendor/github.com/moov-io/base/admin/admin.go
+++ vendor/github.com/moov-io/base/admin/admin.go
@@ -1,23 +1,67 @@
-// Copyright 2018 The ACH Authors
+// Copyright 2018 The Moov Authors
 // Use of this source code is governed by an Apache License
 // license that can be found in the LICENSE file.
 
-// admin implements an http.Server which can be used for operations
+// Package admin implements an http.Server which can be used for operations
 // and monitoring tools. It's designed to be shipped (and ran) inside
 // an existing Go service.
 package admin
 
 import (
+	"context"
 	"fmt"
 	"net/http"
 	"net/http/pprof"
 	"os"
 	"runtime"
 	"strings"
+	"time"
 
 	"github.com/gorilla/mux"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
+
+// NewServer returns an admin Server instance that handles Prometheus metrics
+// and pprof requests.
+func NewServer(addr string) *Server {
+	timeout, _ := time.ParseDuration("45s")
+	return &Server{
+		svc: &http.Server{
+			Addr:         addr,
+			Handler:      Handler(),
+			ReadTimeout:  timeout,
+			WriteTimeout: timeout,
+			IdleTimeout:  timeout,
+		},
+	}
+}
+
+// Server represents a holder around a net/http Server which
+// is used for admin endpoints. (i.e. metrics, healthcheck)
+type Server struct {
+	svc *http.Server
+}
+
+// BindAddr returns the server's bind address. This is in Go's format so :8080 is valid.
+func (s *Server) BindAddr() string {
+	return s.svc.Addr
+}
+
+// Listen brings up the admin HTTP server. This call blocks until the server is Shutdown or panics.
+func (s *Server) Listen() error {
+	if s == nil || s.svc == nil {
+		return nil
+	}
+	return s.svc.ListenAndServe()
+}
+
+// Shutdown unbinds the HTTP server.
+func (s *Server) Shutdown() {
+	if s == nil || s.svc == nil {
+		return
+	}
+	s.svc.Shutdown(context.TODO())
+}
 
 // profileEnabled returns if a given pprof handler should be
 // enabled according to pprofHandlers and the PPROF_* environment

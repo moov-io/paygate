ac88f1d1113f9ef64db8ddc0708f36c97033deba
Adam Shannon <adamkshannon@gmail.com>
2019-02-20 16:24:22 -0600

    depositories,originators: add OFAC check tests

--- originators_test.go
+++ originators_test.go
@@ -7,9 +7,12 @@ package main
 import (
 	"bytes"
 	"encoding/json"
+	"errors"
+	"fmt"
 	"io/ioutil"
 	"net/http"
 	"net/http/httptest"
+	"strings"
 	"testing"
 
 	"github.com/go-kit/kit/log"
@@ -92,5 +95,64 @@ func TestOriginators_getUserOriginators(t *testing.T) {
 	}
 	if originators[0].ID == "" {
 		t.Errorf("originators[0]=%v", originators[0])
+	}
+}
+
+func TestOriginators_OFACMatch(t *testing.T) {
+	db, err := createTestSqliteDB()
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer db.close()
+
+	depRepo := &sqliteDepositoryRepo{db.db, log.NewNopLogger()}
+	origRepo := &sqliteOriginatorRepo{db.db, log.NewNopLogger()}
+
+	// Write Depository to repo
+	userId := nextID()
+	dep := &Depository{
+		ID:            DepositoryID(nextID()),
+		BankName:      "bank name",
+		Holder:        "holder",
+		HolderType:    Individual,
+		Type:          Checking,
+		RoutingNumber: "123",
+		AccountNumber: "151",
+		Status:        DepositoryUnverified,
+	}
+	if err := depRepo.upsertUserDepository(userId, dep); err != nil {
+		t.Fatal(err)
+	}
+
+	rawBody := fmt.Sprintf(`{"defaultDepository": "%s", "identification": "test@example.com", "metadata": "Jane Doe"}`, dep.ID)
+
+	w := httptest.NewRecorder()
+	req := httptest.NewRequest("POST", "/originators", strings.NewReader(rawBody))
+	req.Header.Set("x-user-id", userId)
+
+	// happy path, no OFAC match
+	client := &testOFACClient{}
+	createUserOriginator(client, origRepo, depRepo)(w, req)
+	w.Flush()
+
+	if w.Code != http.StatusOK {
+		t.Errorf("bogus status code: %d: %v", w.Code, w.Body.String())
+	}
+
+	// reset and block via OFAC
+	w = httptest.NewRecorder()
+	client = &testOFACClient{
+		err: errors.New("blocking"),
+	}
+	req.Body = ioutil.NopCloser(strings.NewReader(rawBody))
+	createUserOriginator(client, origRepo, depRepo)(w, req)
+	w.Flush()
+
+	if w.Code != http.StatusBadRequest {
+		t.Errorf("bogus status code: %d: %v", w.Code, w.Body.String())
+	} else {
+		if !strings.Contains(w.Body.String(), `ofac: blocking \"Jane Doe\"`) {
+			t.Errorf("unknown error: %v", w.Body.String())
+		}
 	}
 }

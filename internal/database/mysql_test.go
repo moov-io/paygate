1bf84e84db5b50e8d648ddc78a63a90cdf3aebbd
Adam Shannon <adamkshannon@gmail.com>
2019-05-15 13:13:04 -0500

    internal/database: test UniqueViolation

--- internal/database/mysql_test.go
+++ internal/database/mysql_test.go
@@ -5,6 +5,7 @@
 package database
 
 import (
+	"errors"
 	"testing"
 
 	"github.com/go-kit/kit/log"
@@ -26,5 +27,12 @@ func TestMySQL__mysqlConnection(t *testing.T) {
 	}
 	if len(db.migrations) == 0 {
 		t.Error("expected MySQL migrations")
+	}
+}
+
+func TestMySQLUniqueViolation(t *testing.T) {
+	err := errors.New(`problem upserting depository="282f6ffcd9ba5b029afbf2b739ee826e22d9df3b", userId="f25f48968da47ef1adb5b6531a1c2197295678ce": Error 1062: Duplicate entry '282f6ffcd9ba5b029afbf2b739ee826e22d9df3b' for key 'PRIMARY'`)
+	if !UniqueViolation(err) {
+		t.Error("should have matched unique violation")
 	}
 }
--- internal/database/sqlite_test.go
+++ internal/database/sqlite_test.go
@@ -5,6 +5,7 @@
 package database
 
 import (
+	"errors"
 	"testing"
 
 	"github.com/go-kit/kit/log"
@@ -34,5 +35,12 @@ func TestSQLite__sqliteConnection(t *testing.T) {
 	}
 	if len(db.migrations) == 0 {
 		t.Error("expected SQLite migrations")
+	}
+}
+
+func TestSqliteUniqueViolation(t *testing.T) {
+	err := errors.New(`problem upserting depository="7d676c65eccd48090ff238a0d5e35eb6126c23f2", userId="80cfe1311d9eb7659d02cba9ee6cb04ed3739a85": UNIQUE constraint failed: depositories.depository_id`)
+	if !UniqueViolation(err) {
+		t.Error("should have matched unique violation")
 	}
 }

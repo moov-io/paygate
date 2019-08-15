aba9bc154131948cf9960a63abbd16c421d03310
Adam Shannon <adamkshannon@gmail.com>
2019-08-15 14:06:30 -0500

    files: drop cpu usage (infinite loop) in transfer merging for upload
    This was reported by Ray as he noticed high CPU usage when paygate was
    idle shortly after startup. A quick profile shows this looping was the
    main cause as it didn't properly exit out in error conditions.
    
    For now we're going to remove the errCount condition and just error
    whenever a problem is encountered. If lower levels need retries then
    we can add those on the lower level.

--- file_transfer_async_test.go
+++ file_transfer_async_test.go
@@ -319,6 +319,66 @@ func TestFileTransferController__saveRemoteFiles(t *testing.T) {
 		t.Errorf("deleted file was %s", agent.deletedFile)
 	}
 }
+func TestFileTransferController__filesNearTheirCutoff(t *testing.T) {
+	nyc, _ := time.LoadLocation("America/New_York")
+	now := time.Now().In(nyc)
+
+	dir, err := ioutil.TempDir("", "filesNearTheirCutoff")
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer os.RemoveAll(dir)
+
+	// use a valid file
+	src, err := os.Open(filepath.Join("testdata", "ppd-debit.ach"))
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer src.Close()
+	dst, err := os.Create(filepath.Join(dir, achFilename("987654320", 1)))
+	if err != nil {
+		t.Fatal(err)
+	}
+	if _, err := io.Copy(dst, src); err != nil {
+		t.Fatal(err)
+	}
+
+	// Setup our cutoff time to be "just head" in time
+	cutoffTimes := []*filetransfer.CutoffTime{
+		{
+			RoutingNumber: "987654320",
+			Cutoff:        (now.Hour() * 100) + now.Minute() + 1, // 1 minute in the future in HHmm
+			Loc:           nyc,
+		},
+	}
+
+	outFiles, err := filesNearTheirCutoff(cutoffTimes, dir)
+	if err != nil {
+		t.Error(err)
+	}
+	if len(outFiles) != 1 {
+		t.Fatalf("got %d files, expected one file for upload", len(outFiles))
+	}
+	if outFiles[0] == nil || outFiles[0].File == nil {
+		t.Fatalf("outFiles[0]=%#v", outFiles[0])
+	}
+
+	// verify the file exists (for the sad path test)
+	fds, _ := ioutil.ReadDir(dir)
+	if len(fds) != 1 {
+		t.Errorf("got %d files", len(fds))
+	}
+
+	// bump out time ahead
+	cutoffTimes[0].Cutoff += 100 // add one hour
+	outFiles, err = filesNearTheirCutoff(cutoffTimes, dir)
+	if err != nil {
+		t.Error(err)
+	}
+	if len(outFiles) != 0 {
+		t.Fatal("expected no files (as cutoff is far enough forward in time)")
+	}
+}
 
 func TestFileTransferController__mergeTransfer(t *testing.T) {
 	// build a mergableFile from an example WEB entry

e93033b9c288acc335561d164cec9e3b77bf4702
Adam Shannon <adamkshannon@gmail.com>
2019-08-20 14:52:51 -0500

    micro-deposits: test around depositoryRepository errors in HTTP routes

--- microDeposits_test.go
+++ microDeposits_test.go
@@ -240,6 +240,56 @@ func TestMicroDeposits__repository(t *testing.T) {
 	check(t, &sqliteDepositoryRepo{mysqlDB.DB, log.NewNopLogger()})
 }
 
+func TestMicroDeposits__initiateError(t *testing.T) {
+	id, userID := DepositoryID(base.ID()), base.ID()
+	depRepo := &mockDepositoryRepository{err: errors.New("bad error")}
+	router := &depositoryRouter{
+		logger:         log.NewNopLogger(),
+		depositoryRepo: depRepo,
+	}
+	r := mux.NewRouter()
+	router.registerRoutes(r, true) // disable Accounts service calls
+
+	w := httptest.NewRecorder()
+	req := httptest.NewRequest("POST", fmt.Sprintf("/depositories/%s/micro-deposits", id), nil)
+	req.Header.Set("x-user-id", userID)
+	r.ServeHTTP(w, req)
+	w.Flush()
+
+	if w.Code != http.StatusBadRequest {
+		t.Errorf("bogus HTTP status %d: %s", w.Code, w.Body.String())
+	}
+}
+
+func TestMicroDeposits__confirmError(t *testing.T) {
+	id, userID := DepositoryID(base.ID()), base.ID()
+	depRepo := &mockDepositoryRepository{err: errors.New("bad error")}
+	router := &depositoryRouter{
+		logger:         log.NewNopLogger(),
+		depositoryRepo: depRepo,
+	}
+	r := mux.NewRouter()
+	router.registerRoutes(r, true) // disable Accounts service calls
+
+	var buf bytes.Buffer
+	err := json.NewEncoder(&buf).Encode(confirmDepositoryRequest{
+		Amounts: []string{"USD 0.11"}, // doesn't matter as we error anyway
+	})
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	w := httptest.NewRecorder()
+	req := httptest.NewRequest("POST", fmt.Sprintf("/depositories/%s/micro-deposits/confirm", id), &buf)
+	req.Header.Set("x-user-id", userID)
+	r.ServeHTTP(w, req)
+	w.Flush()
+
+	if w.Code != http.StatusBadRequest {
+		t.Errorf("bogus HTTP status %d: %s", w.Code, w.Body.String())
+	}
+}
+
 func TestMicroDeposits__routes(t *testing.T) {
 	t.Parallel()
 

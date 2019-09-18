5fa1db9520eeff4f2b6bb5219fa3b4670d3b5ffa
Adam Shannon <adamkshannon@gmail.com>
2019-09-18 11:21:04 -0500

    files: test HTTP routes and that their channels get messages

--- file_transfer_sync_test.go
+++ file_transfer_sync_test.go
@@ -13,7 +13,97 @@ import (
 	"github.com/go-kit/kit/log"
 )
 
-func TestForceFileUpload(t *testing.T) {
+func TestFlushIncomingFiles(t *testing.T) {
+	svc := admin.NewServer(":0")
+	go func(t *testing.T) {
+		if err := svc.Listen(); err != nil && err != http.ErrServerClosed {
+			t.Fatal(err)
+		}
+	}(t)
+	defer svc.Shutdown()
+
+	flushIncoming := make(chan struct{}, 1)
+	AddFileTransferSyncRoute(log.NewNopLogger(), svc, flushIncoming, nil)
+
+	// invalid request, wrong HTTP verb
+	req, err := http.NewRequest("GET", "http://localhost"+svc.BindAddr()+"/files/flush/incoming", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		t.Fatal(err)
+	}
+	resp.Body.Close()
+
+	if resp.StatusCode != http.StatusBadRequest {
+		t.Errorf("bogus HTTP status: %d", resp.StatusCode)
+	}
+
+	// valid request
+	req, err = http.NewRequest("POST", "http://localhost"+svc.BindAddr()+"/files/flush/incoming", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+	resp, err = http.DefaultClient.Do(req)
+	if err != nil {
+		t.Fatal(err)
+	}
+	resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		t.Errorf("bogus HTTP status: %d", resp.StatusCode)
+	}
+
+	<-flushIncoming
+}
+
+func TestFlushOutgoingFiles(t *testing.T) {
+	svc := admin.NewServer(":0")
+	go func(t *testing.T) {
+		if err := svc.Listen(); err != nil && err != http.ErrServerClosed {
+			t.Fatal(err)
+		}
+	}(t)
+	defer svc.Shutdown()
+
+	flushOutgoing := make(chan struct{}, 1)
+	AddFileTransferSyncRoute(log.NewNopLogger(), svc, nil, flushOutgoing)
+
+	// invalid request, wrong HTTP verb
+	req, err := http.NewRequest("GET", "http://localhost"+svc.BindAddr()+"/files/flush/outgoing", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		t.Fatal(err)
+	}
+	resp.Body.Close()
+
+	if resp.StatusCode != http.StatusBadRequest {
+		t.Errorf("bogus HTTP status: %d", resp.StatusCode)
+	}
+
+	// valid request
+	req, err = http.NewRequest("POST", "http://localhost"+svc.BindAddr()+"/files/flush/outgoing", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+	resp, err = http.DefaultClient.Do(req)
+	if err != nil {
+		t.Fatal(err)
+	}
+	resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		t.Errorf("bogus HTTP status: %d", resp.StatusCode)
+	}
+
+	<-flushOutgoing
+}
+
+func TestFlushFilesUpload(t *testing.T) {
 	svc := admin.NewServer(":0")
 	go func(t *testing.T) {
 		if err := svc.Listen(); err != nil && err != http.ErrServerClosed {

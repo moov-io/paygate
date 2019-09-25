e50a0bcfaa9535ae0d824db6b7b6d21941d7f12a
Adam Shannon <adamkshannon@gmail.com>
2019-09-25 13:28:29 -0500

    cmd/server: test new main() helpers

--- cmd/server/main_test.go
+++ cmd/server/main_test.go
@@ -13,6 +13,17 @@ import (
 	"github.com/go-kit/kit/log"
 )
 
+func TestMain__setupACHClient(t *testing.T) {
+	logger := log.NewNopLogger()
+	svc := admin.NewServer(":0")
+	httpClient := &http.Client{}
+
+	client := setupACHClient(logger, svc, httpClient)
+	if client == nil {
+		t.Error("expected ACH client")
+	}
+}
+
 func TestMain__setupAccountsClient(t *testing.T) {
 	logger := log.NewNopLogger()
 	svc := admin.NewServer(":0")
@@ -25,5 +36,49 @@ func TestMain__setupAccountsClient(t *testing.T) {
 	client = setupAccountsClient(logger, svc, httpClient, "", "")
 	if client == nil {
 		t.Error("expected non-nil AccountsClient")
+	}
+}
+
+func TestMain__setupFEDClient(t *testing.T) {
+	logger := log.NewNopLogger()
+	svc := admin.NewServer(":0")
+	httpClient := &http.Client{}
+
+	client := setupFEDClient(logger, svc, httpClient)
+	if client == nil {
+		t.Error("expected FED client")
+	}
+}
+
+func TestMain__setupODFIAccount(t *testing.T) {
+	logger := log.NewNopLogger()
+	svc := admin.NewServer(":0")
+	httpClient := &http.Client{}
+
+	accountsClient := setupAccountsClient(logger, svc, httpClient, "", "")
+	if accountsClient == nil {
+		t.Fatal("expected an Accounts client")
+	}
+
+	acct := setupODFIAccount(accountsClient)
+	if acct == nil {
+		t.Error("expected ODFI account")
+	}
+}
+
+func TestMain__setupOFACClient(t *testing.T) {
+	logger := log.NewNopLogger()
+	svc := admin.NewServer(":0")
+	httpClient := &http.Client{}
+
+	client := setupOFACClient(logger, svc, httpClient)
+	if client == nil {
+		t.Error("expected OFAC client")
+	}
+}
+
+func TestMain__setupACHStorageDir(t *testing.T) {
+	if dir := setupACHStorageDir(log.NewNopLogger()); dir != "./storage/" {
+		t.Errorf("unexpected ACH storage directory: %s", dir)
 	}
 }

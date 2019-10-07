c239f0e0d7ffbd1edc4ce5896fd29b8cba26f7e5
Adam Shannon <adamkshannon@gmail.com>
2019-10-07 17:01:07 -0500

    internal/filetransfer: cleanup COR code handling

--- internal/filetransfer/correction.go
+++ internal/filetransfer/correction.go
@@ -49,37 +49,32 @@ func updateDepositoryFromChangeCode(logger log.Logger, code *ach.ChangeCode, ed 
 	}
 	corrected := ed.Addenda98.CorrectedData
 	switch code.Code {
+	// Cases where we could probably update data automatically
 	case
 		"C01", // Incorrect DFI Account Number (first 17 of CorrectedData might have better value)
 		"C02", // Incorrect Routing Number (first 9 of CorrectedData might have better value)
-		"C03": // Incorrect Routing Number and Incorrect DFI Account Number (first 9 and 13th-29th) spaces in 10-12
+		"C03", // Incorrect Routing Number and Incorrect DFI Account Number (first 9 and 13th-29th) spaces in 10-12
+		"C04", // Incorrect Individual Name / Receiving Company Name (first 22)
+		"C06", // Incorrect DFI Account Number and Incorrect Transaction Code (first 17, then 21st and 22nd)
+		"C07", // Incorrect Routing Number, Incorrect DFI Account Number, and Incorrect Tranaction Code. (first 9, 10th-26th, 27th-28th)
+		"C09": // Incorrect Individual Identification Number/Incorrect Receiver Identification Number (first 22)
 		// The Depository's account and/or routing number is invalid, so we probably didn't even find one.
-		logger.Log("changeCode", fmt.Sprintf("rejecting depository=%s for changeCode=%s", dep.ID, code.Code))
+		logger.Log("changeCode", fmt.Sprintf("rejecting depository=%s for changeCode=%s (corrected data: '%s')", dep.ID, code.Code, corrected))
 		return depRepo.UpdateDepositoryStatus(dep.ID, internal.DepositoryRejected)
 
-	case "C04": // Incorrect Individual Name / Receiving Company Name (first 22)
-		logger.Log("changeCode", fmt.Sprintf("bad: ed.IndividualName=%s | good: ed.IndividualName=%s", ed.IndividualName, corrected))
+	// Unsupported cases (for now)
+	case
+		"C08": // Incorrect Receiving DFI Identification (IAT Only)
+		logger.Log("changeCode", fmt.Sprintf("rejecting depository=%s for IAT changeCode=%s", dep.ID, code.Code))
+		return depRepo.UpdateDepositoryStatus(dep.ID, internal.DepositoryRejected)
 
-	case "C05": // Incorrect Transaction Code (first 2)
-		logger.Log("changeCode", fmt.Sprintf("bad: ed.TransactionCode=%d | good: ed.TransactionCode=%s", ed.TransactionCode, corrected))
-
-	case "C06": // Incorrect DFI Account Number and Incorrect Transaction Code (first 17, then 21st and 22nd)
-		logger.Log("changeCode", fmt.Sprintf("C06: TODO(adam)"))
-
-	case "C07": // Incorrect Routing Number, Incorrect DFI Account Number, and Incorrect Tranaction Code. (first 9, 10th-26th, 27th-28th)
-		logger.Log("changeCode", fmt.Sprintf("C07: TODO(adam)"))
-
-	case "C08": // Incorrect Receiving DFI Identification (IAT Only)
-		return fmt.Errorf("unimplemented change code %s", code.Code)
-
-	case "C09": // Incorrect Individual Identification Number/Incorrect Receiver Identification Number (first 22)
-		logger.Log("changeCode", fmt.Sprintf("bad: ed.IdentificationNumber=%s | good: ed.IdentificationNumber=%s", ed.IdentificationNumber, corrected))
-
-	case "C13": // Addenda Format Error (unrecoverable)
+	// Internal errors
+	case
+		"C05", // Incorrect Transaction Code (first 2)
+		"C13", // Addenda Format Error (unrecoverable)
+		"C14": // Incorrect SEC Code for Outbound International Payment
+		logger.Log("changeCode", fmt.Sprintf("rejecting depository=%s due to internal error changeCode=%s", dep.ID, code.Code))
 		return fmt.Errorf("unrecoverable problem with Addenda (code=%s)", code.Code)
-
-	case "C14": // Incorrect SEC Code for Outbound International Payment
-		return fmt.Errorf("unimplemented change code %s", code.Code)
 
 	default:
 		return fmt.Errorf("unhandled change code %s", code.Code)

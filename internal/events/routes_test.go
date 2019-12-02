9a13ff5b673add680f0f996150b79d42adef77b4
Adam Shannon <adamkshannon@gmail.com>
2019-12-02 12:41:29 -0600

    events: test individual event route

--- internal/events/routes_test.go
+++ internal/events/routes_test.go
@@ -6,6 +6,8 @@ package events
 
 import (
 	"encoding/json"
+	"fmt"
+	"net/http"
 	"net/http/httptest"
 	"testing"
 
@@ -14,6 +16,7 @@ import (
 	"github.com/moov-io/paygate/pkg/id"
 
 	"github.com/go-kit/kit/log"
+	"github.com/gorilla/mux"
 )
 
 func TestEvents__getUserEvents(t *testing.T) {
@@ -31,15 +34,17 @@ func TestEvents__getUserEvents(t *testing.T) {
 			t.Fatal(err)
 		}
 
-		// happy path
-		w := httptest.NewRecorder()
-		r := httptest.NewRequest("GET", "/events", nil)
-		r.Header.Set("x-user-id", userID.String())
+		router := mux.NewRouter()
+		AddRoutes(log.NewNopLogger(), router, repo)
 
-		getUserEvents(log.NewNopLogger(), repo)(w, r)
+		req, _ := http.NewRequest("GET", "/events", nil)
+		req.Header.Set("x-user-id", userID.String())
+
+		w := httptest.NewRecorder()
+		router.ServeHTTP(w, req)
 		w.Flush()
 
-		if w.Code != 200 {
+		if w.Code != http.StatusOK {
 			t.Errorf("got %d", w.Code)
 		}
 
@@ -52,6 +57,55 @@ func TestEvents__getUserEvents(t *testing.T) {
 		}
 		if events[0].ID == "" {
 			t.Errorf("events[0]=%v", events[0])
+		}
+	}
+
+	// SQLite tests
+	sqliteDB := database.CreateTestSqliteDB(t)
+	defer sqliteDB.Close()
+	check(t, NewRepo(log.NewNopLogger(), sqliteDB.DB))
+
+	// MySQL tests
+	mysqlDB := database.CreateTestMySQLDB(t)
+	defer mysqlDB.Close()
+	check(t, NewRepo(log.NewNopLogger(), mysqlDB.DB))
+}
+
+func TestEvents__getEvent(t *testing.T) {
+	t.Parallel()
+
+	check := func(t *testing.T, repo Repository) {
+		userID := id.User(base.ID())
+		event := &Event{
+			ID:      EventID(base.ID()),
+			Topic:   "testing",
+			Message: "This is a test",
+			Type:    "TestEvent",
+		}
+		if err := repo.WriteEvent(userID, event); err != nil {
+			t.Fatal(err)
+		}
+
+		router := mux.NewRouter()
+		AddRoutes(log.NewNopLogger(), router, repo)
+
+		req, _ := http.NewRequest("GET", fmt.Sprintf("/events/%s", event.ID), nil)
+		req.Header.Set("x-user-id", userID.String())
+
+		w := httptest.NewRecorder()
+		router.ServeHTTP(w, req)
+		w.Flush()
+
+		if w.Code != http.StatusOK {
+			t.Errorf("got %d", w.Code)
+		}
+
+		var wrapper Event
+		if err := json.NewDecoder(w.Body).Decode(&wrapper); err != nil {
+			t.Fatal(err)
+		}
+		if wrapper.ID != event.ID {
+			t.Errorf("wrapper.ID=%s", wrapper.ID)
 		}
 	}
 

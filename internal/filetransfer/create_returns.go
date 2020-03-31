32a85c5b20b3a2f90b27e1c39b751a4c902ce440
Adam Shannon <adamkshannon@gmail.com>
2020-03-31 17:43:19 -0500

    filetransfer: setup our Controller to return files and transfers
    This change adds a setup from a larger PR[0] to create return files and
    upload them to the ODFI.
    
    Issue: https://github.com/moov-io/paygate/issues/430
    
    [0]: https://github.com/moov-io/paygate/pull/446

---
+++ internal/filetransfer/create_returns.go
@@ -0,0 +1,150 @@
+// Copyright 2020 The Moov Authors
+// Use of this source code is governed by an Apache License
+// license that can be found in the LICENSE file.
+
+package filetransfer
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+	"time"
+
+	"github.com/moov-io/ach"
+	"github.com/moov-io/paygate/internal/filetransfer/config"
+
+	"github.com/go-kit/kit/metrics/prometheus"
+	stdprometheus "github.com/prometheus/client_golang/prometheus"
+)
+
+var (
+	returnFilesCreated = prometheus.NewCounterFrom(stdprometheus.CounterOpts{
+		Name: "return_ach_files_created",
+		Help: "Counter of return files created",
+	}, []string{"destination", "origin"}) // , "code"
+)
+
+func (c *Controller) scratchDir() string {
+	dir := filepath.Join(c.rootDir, "scratch")
+	if _, err := os.Stat(dir); os.IsNotExist(err) {
+		os.MkdirAll(dir, 0777) // ensure directory is created
+	}
+	return dir
+}
+
+func (c *Controller) uploadReturnFile(cfg *config.Config, file *ach.File) error {
+	returnFilesCreated.With("destination", file.Header.ImmediateDestination, "origin", file.Header.ImmediateOrigin).Add(1)
+
+	filename, err := config.RenderACHFilename(cfg.FilenameTemplate(), config.FilenameData{
+		RoutingNumber: file.Header.ImmediateDestination,
+		N:             fmt.Sprintf("%d", time.Now().Unix()),
+	})
+	if err != nil {
+		return fmt.Errorf("problem writing routingNumber=%s return file: %v", cfg.RoutingNumber, err)
+	}
+	filename = fmt.Sprintf("return-%s", filename)
+
+	// write and upload return file
+	scratch := c.scratchDir()
+	out := &achFile{
+		File:     file,
+		filepath: filepath.Join(scratch, filename),
+	}
+	if err := out.write(); err != nil {
+		return fmt.Errorf("problem writing file for return: %v", err)
+	}
+	if err := c.maybeUploadFile(out); err != nil {
+		return fmt.Errorf("problem uploading return file: %v", err)
+	}
+	return nil
+}
+
+func (c *Controller) uploadReturnFiles(files []*ach.File) error {
+	for i := range files {
+		cfg := c.findFileTransferConfig(files[i].Header.ImmediateOrigin)
+
+		if err := c.uploadReturnFile(cfg, files[i]); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func returnEntireFile(file *ach.File, code string) ([]*ach.File, error) {
+	var acc []*ach.File
+	var err error
+
+	for i := range file.Batches {
+		entries := file.Batches[i].GetEntries()
+		for j := range entries {
+			f, err := returnEntry(file.Header, file.Batches[i], entries[j], code)
+			if err != nil {
+				return nil, fmt.Errorf("problem returning=%s traceNumber=%s", code, entries[j].TraceNumber)
+			}
+			acc, err = ach.MergeFiles(append(acc, f))
+			if err != nil {
+				return nil, fmt.Errorf("problem merging return files: %v", err)
+			}
+		}
+	}
+
+	// Flatten each batch in each returned file before uploading.
+	for i := range acc {
+		acc[i], err = acc[i].FlattenBatches()
+		if err != nil {
+			err = fmt.Errorf("problem flattening: %v", err)
+		}
+	}
+
+	return acc, err
+}
+
+func returnEntry(fh ach.FileHeader, b ach.Batcher, entry *ach.EntryDetail, code string) (*ach.File, error) {
+	returnCode := ach.LookupReturnCode(code)
+	if returnCode == nil {
+		return nil, fmt.Errorf("unknown return code: %s", code)
+	}
+
+	addenda99 := ach.NewAddenda99()
+	addenda99.ReturnCode = returnCode.Code
+	addenda99.OriginalTrace = entry.TraceNumber
+	addenda99.OriginalDFI = entry.RDFIIdentification
+	// set TraceNumber as ODFI (we are returning)
+	// addenda99.TraceNumber = remoteach.CreateTraceNumber(fh.ImmediateDestination) // TODO(adam): export?
+
+	file := ach.NewFile()
+	file.Header = fh
+
+	// swap origin / destination
+	file.Header.ImmediateOrigin = fh.ImmediateDestination
+	file.Header.ImmediateOriginName = fh.ImmediateDestinationName
+	file.Header.ImmediateDestination = fh.ImmediateOrigin
+	file.Header.ImmediateDestinationName = fh.ImmediateOriginName
+
+	now := time.Now()
+	file.Header.FileCreationDate = now.Format("060102") // YYMMDD
+	file.Header.FileCreationTime = now.Format("1504")   // HHMM
+
+	batchHeader := b.GetHeader()
+	batchHeader.EffectiveEntryDate = now.Format("060102") // YYMMDD
+
+	// Adjust EntryDetail we're going to return
+	// entry.RDFIIdentification = remoteach.ABA8(file.Header.ImmediateDestination) // TODO(adam): export?
+	// entry.CheckDigit = remoteach.ABACheckDigit(file.Header.ImmediateDestination) // TODO(adam): export?
+
+	batch, err := ach.NewBatch(batchHeader)
+	if err != nil {
+		return nil, err
+	}
+	batch.AddEntry(entry)
+	if err := batch.Create(); err != nil {
+		return nil, err
+	}
+
+	file.AddBatch(batch)
+	if err := file.Create(); err != nil {
+		return nil, err
+	}
+
+	return file, nil
+}
---
+++ internal/filetransfer/create_returns_test.go
@@ -0,0 +1,106 @@
+// Copyright 2020 The Moov Authors
+// Use of this source code is governed by an Apache License
+// license that can be found in the LICENSE file.
+
+package filetransfer
+
+import (
+	"io/ioutil"
+	"path/filepath"
+	"strings"
+	"testing"
+
+	"github.com/moov-io/ach"
+)
+
+func TestController__uploadReturnFile(t *testing.T) {
+	controller := setupTestController(t)
+
+	out, err := parseACHFilepath(filepath.Join("..", "..", "testdata", "cor-c01.ach"))
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if err := controller.uploadReturnFiles([]*ach.File{out}); err != nil {
+		if !strings.Contains(err.Error(), "connect: connection refused") {
+			t.Fatalf("unexpected error: %v", err)
+		}
+	}
+
+	fds, err := ioutil.ReadDir(controller.scratchDir())
+	if err != nil {
+		t.Fatal(err)
+	}
+	if len(fds) != 1 {
+		t.Errorf("got %d files", len(fds))
+	}
+
+	file, err := parseACHFilepath(filepath.Join(controller.scratchDir(), fds[0].Name()))
+	if err != nil {
+		t.Fatal(err)
+	}
+	if file == nil {
+		t.Error("got no file")
+	}
+}
+
+func TestController__returnEntireFile(t *testing.T) {
+	in, err := parseACHFilepath(filepath.Join("..", "..", "testdata", "two-micro-deposits.ach"))
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	outFiles, err := returnEntireFile(in, "R01")
+	if err != nil {
+		t.Fatal(err)
+	}
+	if len(outFiles) != 1 {
+		t.Fatalf("got %d unexpected files", len(outFiles))
+	}
+
+	out := outFiles[0]
+	if len(out.Batches) != 1 {
+		t.Errorf("got %d batches", len(out.Batches))
+	}
+
+	entries := outFiles[0].Batches[0].GetEntries()
+	if len(entries) != 6 {
+		t.Errorf("got %d entries", len(entries))
+	}
+}
+
+func TestController__returnEntry(t *testing.T) {
+	in, err := parseACHFilepath(filepath.Join("..", "..", "testdata", "prenote-ppd-debit.ach"))
+	if err != nil {
+		t.Fatal(err)
+	}
+	if len(in.Batches) != 1 {
+		t.Fatalf("batches=%#v", in.Batches)
+	}
+
+	entry := in.Batches[0].GetEntries()[0]
+	out, err := returnEntry(in.Header, in.Batches[0], entry, "R01")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if len(out.Batches) != 1 {
+		t.Fatalf("batches=%#v", out.Batches)
+	}
+	entries := out.Batches[0].GetEntries()
+	if len(entries) != 1 {
+		t.Fatalf("entries=%#v", entries)
+	}
+
+	if ok, err := isPrenoteEntry(entries[0]); !ok || err != nil {
+		t.Errorf("expected prenote entry: %#v", entries[0])
+		t.Error(err)
+	}
+}
+
+func TestController__returnEntryErr(t *testing.T) {
+	var fh ach.FileHeader
+	if _, err := returnEntry(fh, nil, nil, "invalid"); err == nil {
+		t.Error("expected error")
+	}
+}

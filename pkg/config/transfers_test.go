a24864a32953489c576508d2ab191fdfde67e554
Adam Shannon <adamkshannon@gmail.com>
2020-07-22 14:14:55 -0500

    config: test transfer limits

---
+++ pkg/config/transfers_test.go
@@ -0,0 +1,52 @@
+// Copyright 2020 The Moov Authors
+// Use of this source code is governed by an Apache License
+// license that can be found in the LICENSE file.
+
+package config
+
+import (
+	"testing"
+
+	"github.com/moov-io/paygate/pkg/model"
+)
+
+func TestFixedLimits__Soft(t *testing.T) {
+	cfg := &FixedLimits{
+		SoftLimit: "USD 104.23",
+	}
+	amt, _ := model.NewAmount("USD", "101.00")
+	if over, err := cfg.OverSoftLimit(amt); over || err != nil {
+		t.Errorf("expected amount to pass: %v", err)
+	}
+
+	// exceed limit
+	amt, _ = model.NewAmount("USD", "120.01")
+	if over, err := cfg.OverSoftLimit(amt); !over || err != nil {
+		t.Errorf("expected error: %v", err)
+	}
+}
+
+func TestFixedLimits__Hard(t *testing.T) {
+	cfg := &FixedLimits{
+		HardLimit: "USD 104.23",
+	}
+	amt, _ := model.NewAmount("USD", "101.00")
+	if over, err := cfg.OverHardLimit(amt); over || err != nil {
+		t.Errorf("expected amount to pass: %v", err)
+	}
+
+	// exceed limit
+	amt, _ = model.NewAmount("USD", "120.01")
+	if over, err := cfg.OverHardLimit(amt); !over || err != nil {
+		t.Errorf("expected error: %v", err)
+	}
+}
+
+func TestFixedLimitsErr(t *testing.T) {
+	cfg := &FixedLimits{
+		SoftLimit: "invalid",
+	}
+	if _, err := cfg.overLimit(cfg.SoftLimit, nil); err == nil {
+		t.Fatal("expected error")
+	}
+}

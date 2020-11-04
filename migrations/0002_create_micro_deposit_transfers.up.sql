b44896fe577972dac10d461491ce764d8665bdd0
Pavel Gabriel <alovak@gmail.com>
2020-11-04 18:38:18 +0100

    unify setting PRIMARY KEY in /migrations

--- migrations/0002_create_micro_deposit_transfers.up.sql
+++ migrations/0002_create_micro_deposit_transfers.up.sql
@@ -1,5 +1,4 @@
 CREATE TABLE `micro_deposit_transfers` (
   `micro_deposit_id` varchar(40) NOT NULL,
-  `transfer_id` varchar(40) NOT NULL,
-  PRIMARY KEY (`transfer_id`)
+  `transfer_id` varchar(40) PRIMARY KEY NOT NULL
 )

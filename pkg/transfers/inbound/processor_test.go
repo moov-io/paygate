2b2548ad524b0aab434832512dfb45810501f1df
Alec Fong <alecfong1@gmail.com>
2021-03-08 10:16:42 -0800

    Allowing processor to handle files downloaded to root of temp dir

--- pkg/transfers/inbound/processor_test.go
+++ pkg/transfers/inbound/processor_test.go
@@ -21,7 +21,7 @@ func TestProcessor__process(t *testing.T) {
 	// By reading a file without ACH FileHeaders we still want to try and process
 	// Batches inside of it if any are found, so reading this kind of file shouldn't
 	// return an error from reading the file.
-	if err := process(dir, processors); err != nil {
+	if err := ProcessFiles(&downloadedFiles{dir: dir}, processors); err != nil {
 		t.Error(err)
 	}
 }
